ec0b9d22452c1194e64a67a17a3dd58fd21cb64a
k5md <serg.dfc@yandex.ru>
2019-10-24 21:16:28 +0300

    make whole button trigger input file

--- src/sidebar/components/FileInput/index.tsx
+++ src/sidebar/components/FileInput/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { uniqueId } from 'lodash';
 import { AnchorButton, Portal } from '@blueprintjs/core';
 
@@ -7,19 +7,18 @@ import * as classes from './styles.scss';
 export const FileInput = ({
   onInputChange,
   label,
-  inputProps,
   ...containerProps,
 }) => {
   const [ id ] = useState(() => uniqueId());
+  const inputRef = useRef(null);
 
   // NOTE: use portal here to cover the case when fileInput is being used in popover children
   return (
     <div {...containerProps}>
-      <AnchorButton minimal className={classes.inputContainer}>
+      <AnchorButton minimal text={label} onClick={() => inputRef.current.click()}>
         <Portal>
-          <input type="file" id={id} className={classes.inputElement} onChange={onInputChange} {...inputProps}/>
-        </Portal>
-        <label for={id}>{label}</label>
+          <input ref={inputRef} type="file" id={id} className={classes.inputElement} onChange={onInputChange} {...inputProps}/>
+        </Portal>   
       </AnchorButton>
     </div>
   );
