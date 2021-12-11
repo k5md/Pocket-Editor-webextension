ed00e8851994c69bfde31a39595efb05e0136d7c
k5md <serg.dfc@yandex.ru>
2021-12-11 18:15:16 +0300

    fix linting issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,11 +2,23 @@ const path = require('path');
 
 module.exports = {
   parser: '@typescript-eslint/parser', // Specifies the ESLint parser
-  plugins: ["@typescript-eslint"],
+  plugins: ['@typescript-eslint'],
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended",
+    'eslint:recommended',
+    'plugin:react/recommended',
+  ],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+      ],
+      parser: '@typescript-eslint/parser',
+      plugins: ['@typescript-eslint']
+    }
   ],
   parserOptions: {
     project: path.resolve(__dirname, './tsconfig.json'),
@@ -14,12 +26,13 @@ module.exports = {
     ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
     sourceType: 'module', // Allows for the use of imports
   },
-  rules: {
-
-  },
   settings:  {
     react:  {
       version:  'detect',  // Tells eslint-plugin-react to automatically detect the version of React to use
     },
+  },
+  env: {
+    browser: true,
+    webextensions: true,
   },
 };
